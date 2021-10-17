72fdb20e1945378db05e19c9b5f3e8ccb2b70cb2
wmokrogulski <mokrogulski1b@gmail.com>
2021-10-17 09:01:28 +0200

    Ćwiczenie 1 clean up

--- cwiczenie1/zadanie2/zadanie 2.py
+++ cwiczenie1/zadanie2/zadanie 2.py
@@ -5,25 +5,26 @@ x1 = cp.Variable()  # ilość płatków
 x2 = cp.Variable()  # ilość mleka
 x3 = cp.Variable()  # ilość chleba
 
-objective = cp.Minimize(.15 * x1 + .25 * x2 + .05 * x3)  # koszty
+costs = .15 * x1 + .25 * x2 + .05 * x3
+objective = cp.Minimize(costs)  # koszty
 
 calories_constraints = [
-    2250 >= 70 * x1 + 121 * x2 + 65 * x3,
+    70 * x1 + 121 * x2 + 65 * x3 <= 2250,
     70 * x1 + 121 * x2 + 65 * x3 >= 2000
 ]
 vitamines_constraints = [
-    1e4 >= 107 * x1 + 500 * x2 + 0 * x3,
+    107 * x1 + 500 * x2 + 0 * x3 <= 1e4,
     107 * x1 + 500 * x2 + 0 * x3 >= 5e3
 ]
 sugar_constraint = [
     45 * x1 + 40 * x2 + 60 * x3 <= 1000
 ]
 quantity_constraints = [
-    10 >= x1,
+    x1 <= 10,
     x1 >= 0,
-    10 >= x2,
+    x2 <= 10,
     x2 >= 0,
-    10 >= x3,
+    x3 <= 10,
     x3 >= 0
 ]
 constraints = []
